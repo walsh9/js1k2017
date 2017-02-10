c88c78daeb7a10b870fee8bff57dc2b2f7a0cabd
walsh9 <walsh9@gmail.com>
2017-02-10 18:48:30 -0500

    smaller

--- demo.js
+++ demo.js
@@ -4,89 +4,86 @@ W = a.width
 H = a.height
 M = Math
 R = M.random
-cls=_=>{c.clearRect(0,0,W,H)};
-shf=l=>l.map((v,i)=>l[l[i]=l[j=0|i+R()*(l.length-i)],j]=v)
+_='length'
 
-$ = ch => {
-  c.font = `${S}px serif`;
-  c.textBaseline = "top";  
+cls=_=>a.width=W
+
+$=ch=> {
+  c.font = `${S}px serif`
+  c.textBaseline = "top"  
   c.fillText(ch, 0, 0);
   im = c.getImageData(0, 0, S, S);
-  cls();
-  gs = [];
-  for (i = im.data.length; i-=4;) {
+  cls()
+  gs = []
+  for (i = im.data[_]; i-=4;) {
     [rc, gc, bc] = im.data.slice(i, i+3);
-    lum = 0.2126 * rc/255 + 0.7152 * gc/255 + 0.0722 * bc/255;
-    li = ~~((116 * M.pow(lum, 1/3) - 16)/100 * 255);
-    gs[i/4] = li;
+    lum = .21 * rc/255 + .72 * gc/255 + .07 * bc/255
+    li = ~~((116 * M.pow(lum, 1/3) - 16)/99 * 255)
+    gs[i/4] = li
   }
   gs.forEach((px, i) => {
     opx = px;
-    npx = px > 128 ? 255 : 0;
-    q = opx - npx;
-    gs[i] = npx;
-    dx = i % S;
-    dy = ~~(i / S);
-    [[ 1,0,7/16],
-     [-1,1,3/16],
-     [ 0,1,5/16],
-     [ 1,1,1/16]].forEach(dith => {
-      dx += dith[0];
-      dy += dith[1];
+    npx = px > 128 ? 255 : 0
+    qe = opx - npx
+    gs[i] = npx
+    ex = i % S
+    ey = ~~(i / S);
+    [[ 1,0,.44],
+     [-1,1,.19],
+     [ 0,1,.31],
+     [ 1,1,.06]].forEach(dith => {
+      dx = ex + dith[0];
+      dy = ey + dith[1];
       if(dx < S && dx >= 0 && dy < S) 
-        gs[dx + dy * S] += M.round(q * dith[2])
+        gs[dx + dy * S] += (qe * dith[2])+.5|0 
     });
   });
   pt = [];
-  for(i = gs.length; i--;) { 
-    if(gs[i] == 0 && im.data[i*4+3] > 99)
+  for(i = gs[_]; i--;) { 
+    if(!gs[i] && im.data[i*4+3] > 128)
       pt.push([i % S, ~~(i/S)])
   }
-  shf(pt)
+  pad(pt)
   return pt
 }
 
 ct = (p1, p2) => {
-  diff = p1.length - p2.length;
-  if (diff > 0)
-    pad(p2, diff);
-  else
-    pad(p1, M.abs(diff));
+  diff = p1[_] - p2[_]
+  pad(diff > 0 ? p2 : p1, M.abs(diff));
   return p1.map((_,i) => [p1[i][0], p1[i][1], p2[i][0], p2[i][1]]);
 }
 
-pad=(particles, n)=>{
+pad=(ptx, n)=>{
   for (;n--;) {
-    particles.push(particles[~~(R() * particles.length)]);
+    ptx.push(ptx[~~(R() * ptx[_])]);
   }
+  ptx.map((v,i)=>ptx[ptx[i]=ptx[j=0|i+R()*(ptx[_]-i)],j]=v)
 };
 
 D=99;
 ani = (b,e) => b+(e-b)*T/D
 
-draw = (ps, x=0, y=0) => {
+draw = (ps, x=20, y=20) => {
   cls()
   c.fillStyle = '#f0f';
   ps.forEach((p) => {
-    c.fillRect(~~(ani(p[0], p[2]) + x), ~~(ani(p[1], p[3]) + y), 1, 1);
+    c.fillRect(~~(ani(p[0], p[2]) + x)*2, ~~(ani(p[1], p[3]) + y)*2, 2, 2);
   });
-  if (T >= D) {
-    T = 0;
-    STAGE++;
-  }
+  if (T >= D)
+    T = -99,STAGE++
 }
 
-e = [..."🐇🎩🕊🐣🎩👋"].map($)
+e = [..."👾🔮"].map($)
 t = []
-len = e.length
+len = e[_]
 for(x=len-1;x--;)
   t.push(ct(e[x+1], e[x]))
 t.push(ct(e[0], e[len-1]))
 
 setInterval(_=>{
   T++
-  if (STAGE < len)
-    draw(t[STAGE], 20, 20);
-  else
-    STAGE=T=0
+  if (STAGE < len && T > 0)
+      draw(t[STAGE]);
+  else if (STAGE >= len)
+    STAGE=0,T=-99
 }, 32);
